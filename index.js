313882b51c05ca067a1d9beff3cf3a2c9e6eab00
Kaustubh Singh <kaustubh.singh@gmail.com>
2017-05-21 08:49:54 -0400

    update

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var path = require('path');
+var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+const port = process.env.PORT || '5000';
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+app.set('port', port);
+app.use(express.static(__dirname + '/public'));
+
+app.get('/[^\.]+$', function(req, res) {
+    res.set('Content-Type', 'text/html')
+        .sendFile(path.join(__dirname, '/public/index.html'))
+});
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+});
